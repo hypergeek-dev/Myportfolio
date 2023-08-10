3d9a7702612bc3525bb5dc4ecba747d5479e7a80
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-11 00:29:15 +0200

    Fixing src bug in nav as module

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Navbar from './Navbar';
+import Navbar from './modules/navbar/';
 import Home from './modules/home/';
 import About from './modules/about/';
 import Projects from './modules/projects/';
