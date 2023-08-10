9f1024a53e4a08ccc4d993820a41b14197319453
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-10 22:57:55 +0200

    Import bootstrap src

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './modules/home/';
 import About from './modules/about/';
