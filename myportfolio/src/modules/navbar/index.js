1baae3f189ab4216a46c504627109d882a00a666
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-12 00:42:49 +0200

    Adding global css

--- myportfolio/src/modules/navbar/index.js
+++ myportfolio/src/modules/navbar/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
