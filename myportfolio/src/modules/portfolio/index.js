4f61e533c9685a69a598bf12d0a5aa8701860220
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-16 23:48:06 +0200

    portfolio

--- myportfolio/src/modules/portfolio/index.js
+++ myportfolio/src/modules/portfolio/index.js
@@ -1,16 +1,39 @@
-// modules/projects/index.js
 import React from 'react';
+
+const soloProjects = [
+  { imageUrl: 'path/to/screenshot1.jpg', description: 'Description 1' },
+  { imageUrl: 'path/to/screenshot2.jpg', description: 'Description 2' },
+  // ... other projects ...
+];
+
+const colloquialProjects = [
+  { imageUrl: 'path/to/screenshotA.jpg', description: 'Description A' },
+  // ... other projects ...
+];
 
 const Portfolio = () => {
   return (
-    <div>
-      <h1>My Projects</h1>
-      <div>
-        <h2>Project Name</h2>
-        <p>Short description of the project.</p>
-        <a href="live_link">View Live</a> | <a href="source_code_link">Source Code</a>
- </div>
-    </div>
+    <>
+      <div className="d-flex p-2">
+        <h1>Solo projects</h1>
+      </div>
+      {soloProjects.map((project, index) => (
+        <div key={index} className="d-inline-flex p-2">
+          <img src={project.imageUrl} alt={`Solo Project ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
+          <div className="d-flex p-2">{project.description}</div>
+        </div>
+      ))}
+
+      <div className="d-flex p-2">
+        <h1>Colloquial projects</h1>
+      </div>
+      {colloquialProjects.map((project, index) => (
+        <div key={index} className="d-inline-flex p-2">
+          <img src={project.imageUrl} alt={`Colloquial Project ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
+          <div className="d-flex p-2">{project.description}</div>
+        </div>
+      ))}
+    </>
   );
 };
 
