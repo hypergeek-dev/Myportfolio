9290ff25c6e9f935096565650979a9748c4a6394
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-28 10:29:03 +0200

    Styling footer

--- myportfolio/src/modules/footer/index.js
+++ myportfolio/src/modules/footer/index.js
@@ -4,24 +4,24 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './footer.css';
-
 const Footer = () => {
   return (
-    <footer className="mt-5">
+    <footer className="mt-5 footer-container">  
       <div className="d-flex justify-content-center">
         <a href="#" className="social-icon mx-2">
-          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-linkedin-in"></i>
         </a>
         <a href="#" className="social-icon mx-2">
-          <i className="fab fa-twitter"></i>
+          <i className="fab fa-github"></i>
         </a>
         <a href="#" className="social-icon mx-2">
-          <i className="fab fa-instagram"></i>
+          <i className="fas fa-envelope"></i>
         </a>
       </div>
     </footer>
   );
 };
+
 
 
 export default Footer;
