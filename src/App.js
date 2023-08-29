59b27c39533058494330e188f912183a29f447ad
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-08-29 13:33:10 +0200

    Reversing src fixing. Didn't correct it.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from '../node_modules/react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './modules/navbar/';
 import Home from './modules/home/';
