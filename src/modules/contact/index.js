44c0ac859c6f1a231926cc51f53a6444dfc51f0a
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-09-04 08:45:02 +0200

    Css styling contact form

--- src/modules/contact/index.js
+++ src/modules/contact/index.js
@@ -1,34 +1,48 @@
-// modules/contact/index.js
-import React from 'react';
-
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6erq9js', 'template_b96h8du', form.current, '3Yvs7CV-qUSayuQjb')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send message. Please try again later.');
+      });
+  };
+
   return (
     <section id="contact">
       <h1 className="section-header">Contact</h1>
       <div className="contact-wrapper">
-        <form id="contact-form" className="form-horizontal">
+        <form ref={form} id="contact-form" className="form-horizontal" onSubmit={sendEmail}>
           <div className="form-group">
             <div className="col-sm-12">
-              <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value="Name" required />
+              <input type="text" className="form-control" id="name" name="name" placeholder="Name" required />
             </div>
           </div>
 
           <div className="form-group">
             <div className="col-sm-12">
-              <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" value="E-mail" required />
+              <input type="email" className="form-control" id="email" name="email" placeholder="E-mail" required />
             </div>
           </div>
 
-          <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message" value="Message" required />
+          <textarea className="form-control" rows="10" name="message" placeholder="Message" required />
 
-          <button className="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-  <div className="button-wrapper">  
-    <div className="alt-send-button">
-      <i className="fa fa-paper-plane"></i><span className="send-text">Send</span>
-    </div>
-  </div>
-</button>
+          <button className="btn send-button" id="submit" type="submit">
+            <div className="button-wrapper">  
+              <div className="alt-send-button">
+                <span className="send-text">Send</span>
+              </div>
+            </div>
+          </button>
         </form>
       </div>
     </section>
